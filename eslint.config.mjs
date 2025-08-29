42d0f410211f2421dbbc9acea884a5a8fc4469d1
ibrahim <henryvercel@gmail.com>
2025-08-29 09:46:45 +0100

    updated pages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // 🚨 Disable "no-explicit-any" everywhere
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
